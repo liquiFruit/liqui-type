579123ea86c5e1102dd5f1a0496f4bdf86be6dac
John P <johnrprutton@gmail.com>
2023-12-19 21:52:08 +0200

    feat: make the test restart itself

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,301 @@
 
 import { useEffect, useState } from "react"
 
+const words = {
+  easy: [
+    "the",
+    "and",
+    "you",
+    "that",
+    "was",
+    "for",
+    "are",
+    "with",
+    "his",
+    "they",
+    "have",
+    "this",
+    "from",
+    "one",
+    "had",
+    "word",
+    "but",
+    "not",
+    "what",
+    "all",
+    "were",
+    "we",
+    "when",
+    "your",
+    "can",
+    "said",
+    "there",
+    "use",
+    "an",
+    "each",
+    "which",
+    "she",
+    "do",
+    "how",
+    "their",
+    "if",
+    "will",
+    "up",
+    "other",
+    "about",
+    "out",
+    "many",
+    "then",
+    "them",
+    "these",
+    "so",
+    "some",
+    "her",
+    "would",
+    "make",
+    "like",
+    "him",
+    "into",
+    "time",
+    "has",
+    "look",
+    "two",
+    "more",
+    "write",
+    "see",
+    "number",
+    "way",
+    "could",
+    "people",
+    "than",
+    "first",
+    "water",
+    "been",
+    "call",
+    "who",
+    "oil",
+    "its",
+    "now",
+    "find",
+    "long",
+    "down",
+    "day",
+    "did",
+    "get",
+    "come",
+    "made",
+    "may",
+    "part",
+    "over",
+    "new",
+    "sound",
+    "take",
+    "only",
+    "little",
+    "work",
+    "know",
+    "place",
+    "year",
+    "live",
+    "me",
+    "back",
+    "give",
+    "most",
+    "very",
+  ],
+
+  medium: [
+    "although",
+    "beginning",
+    "children",
+    "develop",
+    "everybody",
+    "friendship",
+    "happiness",
+    "important",
+    "knowledge",
+    "literature",
+    "maintenance",
+    "necessary",
+    "operation",
+    "particular",
+    "questioning",
+    "realization",
+    "satisfaction",
+    "themselves",
+    "understand",
+    "vocabulary",
+    "wonderful",
+    "beautiful",
+    "character",
+    "delicious",
+    "elephant",
+    "furniture",
+    "gratitude",
+    "happening",
+    "influence",
+    "journey",
+    "kitchen",
+    "landscape",
+    "measurement",
+    "opportunity",
+    "photography",
+    "quantity",
+    "reflection",
+    "sincerely",
+    "technology",
+    "university",
+    "valuable",
+    "wondering",
+    "yellow",
+    "abundance",
+    "boundary",
+    "celebration",
+    "difference",
+    "efficiency",
+    "flourish",
+    "generation",
+    "harmony",
+    "incredible",
+    "judgment",
+    "knowledgeable",
+    "leadership",
+    "mechanism",
+    "narrative",
+    "occurrence",
+    "parallel",
+    "quintessential",
+    "reliability",
+    "transformation",
+    "unbelievable",
+    "vibrant",
+    "wholesome",
+    "xylophone",
+    "yielding",
+    "zealous",
+    "aberration",
+    "blissful",
+    "capitulate",
+    "dexterity",
+    "effervescent",
+    "flamboyant",
+    "garrulous",
+    "higgledy-piggledy",
+    "iconoclast",
+    "juxtapose",
+    "kaleidoscope",
+    "labyrinthine",
+    "magnanimous",
+    "nebulous",
+    "obfuscate",
+    "peculiar",
+    "quixotic",
+    "recalcitrant",
+    "serendipity",
+    "trepidation",
+    "ubiquitous",
+    "verisimilitude",
+    "whimsical",
+    "xenophobe",
+    "yesteryear",
+    "zeppelin",
+  ],
+
+  hard: [
+    "exaggeration",
+    "quizzically",
+    "labyrinthine",
+    "haphazardly",
+    "bewilderment",
+    "equivocal",
+    "discombobulated",
+    "inscrutable",
+    "juxtaposition",
+    "perfidious",
+    "serendipity",
+    "cacophony",
+    "effervescent",
+    "phosphorescent",
+    "onomatopoeia",
+    "incandescent",
+    "paradoxical",
+    "ephemeral",
+    "lackadaisical",
+    "ubiquitous",
+    "procrastination",
+    "pusillanimous",
+    "idiosyncrasy",
+    "sesquipedalian",
+    "superfluous",
+    "facetious",
+    "garrulous",
+    "mellifluous",
+    "recalcitrant",
+    "antithesis",
+    "grandiloquent",
+    "disquisition",
+    "perspicacious",
+    "ubiquity",
+    "pellucid",
+    "aberration",
+    "capitulate",
+    "circumlocution",
+    "disparate",
+    "ebullient",
+    "fastidious",
+    "gregarious",
+    "iconoclast",
+    "jaundiced",
+    "kaleidoscope",
+    "lugubrious",
+    "magnanimous",
+    "nonchalant",
+    "obfuscate",
+    "quintessential",
+    "supercilious",
+    "tenebrous",
+    "umbrage",
+    "verisimilitude",
+    "welter",
+    "xenophobe",
+    "yammering",
+    "zeppelin",
+    "ambidextrous",
+    "belligerent",
+    "cogent",
+    "defenestration",
+    "quixotic",
+  ],
+}
+function generateRandomSentence({
+  difficulty,
+  length = 5,
+}: {
+  difficulty: "easy" | "medium" | "hard"
+  length?: number
+}) {
+  const _words = words.easy
+  let sentence = ""
+
+  for (let i = 0; i < length; i++) {
+    const r = Math.floor(Math.random() * _words.length)
+
+    sentence += _words[r]
+
+    if (i !== length - 1) sentence += " "
+  }
+
+  return sentence
+}
+
 export default function Home() {
-  const [sentence, setSentence] = useState("hello world")
+  const [sentence, setSentence] = useState("")
   const [userInput, setUserInput] = useState("")
+
+  const generateSentence = () => {
+    setSentence(generateRandomSentence({ difficulty: "easy" }))
+    setUserInput("")
+  }
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
@@ -19,9 +311,12 @@ export default function Home() {
         setUserInput((prev) => prev.substring(0, prev.length - 1))
       } else if (event.key === "Enter") {
         // Handle reset
-        setUserInput("")
+        generateSentence()
       }
     }
+
+    // Generate first sentence
+    generateSentence()
 
     // Add event listener for keydown event
     document.addEventListener("keydown", handleKeyPress)
@@ -31,6 +326,10 @@ export default function Home() {
       document.removeEventListener("keydown", handleKeyPress)
     }
   }, [])
+
+  useEffect(() => {
+    if (userInput.length === sentence.length) generateSentence()
+  }, [userInput, sentence])
 
   return (
     <main className="grid h-[100svh] place-items-center backdrop-blur-xl">
